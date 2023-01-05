b5254e8b26730add397a6cc437a1639c2c23df43
Unieveth <git@unieveth.com>
2023-01-04 20:27:54 -0600

    added endpoint env

--- src/env.ts
+++ src/env.ts
@@ -1,16 +1,17 @@
-import { envsafe, str } from "envsafe";
+import { envsafe, str } from 'envsafe'
 
 export const env = envsafe({
   ACCESS_KEY: str(),
   SECRET_KEY: str(),
   BUCKET: str(),
   NAME_PREFIX: str(),
+  ENDPOINT: str(),
   BACKUP_DATABASE_URL: str({
-    desc: 'The connection string of the database to backup.'
+    desc: 'The connection string of the database to backup.',
   }),
   BACKUP_CRON_SCHEDULE: str({
     desc: 'The cron schedule to run the backup on.',
     default: '0 5 * * *',
-    allowEmpty: true
+    allowEmpty: true,
   }),
 })
