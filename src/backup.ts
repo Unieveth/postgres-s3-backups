01a2ab9e2ab817daaf8cc505df92145ac49f9c88
Unieveth <git@unieveth.com>
2023-01-04 20:18:04 -0600

    moved to minio

--- src/backup.ts
+++ src/backup.ts
@@ -1,66 +1,63 @@
-import { exec } from "child_process";
-import { PutObjectCommand, S3Client, S3ClientConfig } from "@aws-sdk/client-s3";
-import { createReadStream } from "fs";
+import { exec } from 'child_process'
+import Minio from 'minio'
 
-import { env } from "./env";
+import { env } from './env'
 
-const uploadToS3 = async ({ name, path }: {name: string, path: string}) => {
-  console.log("Uploading backup to S3...");
+const uploadToS3 = async ({ name, path }: { name: string; path: string }) => {
+  console.log('Uploading backup to S3...')
 
-  const bucket = env.AWS_S3_BUCKET;
+  const bucket = env.BUCKET
 
-  const clientOptions: S3ClientConfig = {
-    region: env.AWS_S3_REGION,
-  }
+  const client = new Minio.Client({
+    endPoint: process.env.ENDPOINT || '',
+    port: 9000,
+    useSSL: false,
+    accessKey: process.env.ACCESS_KEY || '',
+    secretKey: process.env.SECRET_KEY || '',
+  })
 
-  if (env.AWS_S3_ENDPOINT) {
-    console.log(`Using custom endpoint: ${env.AWS_S3_ENDPOINT}`)
-    clientOptions['endpoint'] = env.AWS_S3_ENDPOINT;
-  }
+  await client.fPutObject(bucket, name, path)
 
-  const client = new S3Client(clientOptions);
+  // await client.send(
+  //   new PutObjectCommand({
+  //     Bucket: bucket,
+  //     Key: name,
+  //     Body: createReadStream(path),
+  //   })
+  // )
 
-  await client.send(
-    new PutObjectCommand({
-      Bucket: bucket,
-      Key: name,
-      Body: createReadStream(path),
-    })
-  )
-
-  console.log("Backup uploaded to S3...");
+  console.log('Backup uploaded to S3...')
 }
 
 const dumpToFile = async (path: string) => {
-  console.log("Dumping DB to file...");
+  console.log('Dumping DB to file...')
 
   await new Promise((resolve, reject) => {
     exec(
       `pg_dump ${env.BACKUP_DATABASE_URL} -F t | gzip > ${path}`,
       (error, stdout, stderr) => {
         if (error) {
-          reject({ error: JSON.stringify(error), stderr });
-          return;
+          reject({ error: JSON.stringify(error), stderr })
+          return
         }
 
-        resolve(undefined);
+        resolve(undefined)
       }
-    );
-  });
+    )
+  })
 
-  console.log("DB dumped to file...");
+  console.log('DB dumped to file...')
 }
 
 export const backup = async () => {
-  console.log("Initiating DB backup...")
+  console.log('Initiating DB backup...')
 
-  let date = new Date().toISOString()
-  const timestamp = date.replace(/[:.]+/g, '-')
-  const filename = `backup-${timestamp}.tar.gz`
-  const filepath = `/tmp/${filename}`
+  const date = new Date()
+  const fileName = env.NAME_PREFIX + date.toUTCString() + '.tar.gz'
+  const filepath = `/tmp/${fileName}`
 
   await dumpToFile(filepath)
-  await uploadToS3({name: filename, path: filepath})
+  await uploadToS3({ name: fileName, path: filepath })
 
-  console.log("DB backup complete...")
+  console.log('DB backup complete...')
 }
--- src/env.ts
+++ src/env.ts
@@ -1,10 +1,10 @@
 import { envsafe, str } from "envsafe";
 
 export const env = envsafe({
-  AWS_ACCESS_KEY_ID: str(),
-  AWS_SECRET_ACCESS_KEY: str(),
-  AWS_S3_BUCKET: str(),
-  AWS_S3_REGION: str(),
+  ACCESS_KEY: str(),
+  SECRET_KEY: str(),
+  BUCKET: str(),
+  NAME_PREFIX: str(),
   BACKUP_DATABASE_URL: str({
     desc: 'The connection string of the database to backup.'
   }),
@@ -12,10 +12,5 @@ export const env = envsafe({
     desc: 'The cron schedule to run the backup on.',
     default: '0 5 * * *',
     allowEmpty: true
-  }),
-  AWS_S3_ENDPOINT: str({
-    desc: 'The S3 custom endpoint you want to use.',
-    default: '',
-    allowEmpty: true,
   }),
 })
